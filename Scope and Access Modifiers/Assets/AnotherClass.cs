2461747e6b68180dd9d958ac1e2da094e437c0c2
Ernie Vilela <ernie_vc@hotmail.com>
2015-09-28 14:45:58 -0600

    Scope And Access Modifiers Commit

---
+++ Scope and Access Modifiers/Assets/AnotherClass.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass
+{
+	public int ten= 10;
+
+	
+	public void Add (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Fruit total: " + answer);
+	}
+	
+	
+	private void Multiply (int a, int b)
+	{
+		int answer;
+		answer = a * b;
+		Debug.Log("Office Supplies total: " + answer);
+	}
+}
---
+++ Scope and Access Modifiers/Assets/ScopeAndAccessModifiers.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccessModifiers : MonoBehaviour
+{
+	public int alpha;
+	
+	
+	private int beta = 0;
+	private int gamma = 5;
+	
+	
+	private AnotherClass myOtherClass;
+	
+	
+	void Start ()
+	{
+		alpha = 29;
+		
+		myOtherClass = new AnotherClass();
+		myOtherClass.Add(alpha, myOtherClass.ten);
+	}
+}
