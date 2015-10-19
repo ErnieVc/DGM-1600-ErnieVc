ec0e4f0e8ac5a53f18b1222e326bd6c01b60e0b3
Ernie Vilela <ernie_vc@hotmail.com>
2015-10-19 15:27:46 -0600

    Arrays commit

---
+++ Arrays/Assets/Arrays.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour {
+
+	//Declaration of an Array
+	int[]myArray= new int[7];
+
+	//Declaring and Initializing an Array
+	int[] anotherArray = {1,2,3,4};
+
+	//Access Int Array from the Inspector
+	public GameObject [] players;
+
+	void Start () 
+	{
+		//Accessing the elements of IntArray
+		myArray [0] = 34;
+		myArray [1] = 4;
+
+		for (int i=0; i < anotherArray.Length; i++) 
+		{
+			Debug.Log ("number in the array" + anotherArray [i]);
+		}
+			
+		
+	}
+	
+
+}
