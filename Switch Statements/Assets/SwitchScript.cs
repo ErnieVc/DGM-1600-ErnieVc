891dda4a4fe9ead53b9ae40f5b1d770aafc9cdd0
Ernie Vilela <ernie_vc@hotmail.com>
2015-11-02 14:36:59 -0700

    Switch Statement commit

---
+++ Switch Statements/Assets/SwitchScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScript : MonoBehaviour 
+{
+public int level=5;
+
+void Start(){
+
+		LevelReading();
+}
+
+void LevelReading()
+{
+switch (level)
+{
+case 1: 
+print("One");
+break;
+
+case 5: 
+print("Five");
+break;
+
+default://else
+print("Default Level");
+break;
+}
+
+}
+
+
+}
