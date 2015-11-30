507dac652ddac0cccb126145e545bd1b31285ae5
Ernie Vilela <ernie_vc@hotmail.com>
2015-11-30 15:05:23 -0700

    Duck C# scripts commit

---
+++ Duck Hunt/Assets/DirectionChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionChanger : MonoBehaviour {
+
+    public enum Changer { Horizontal, Vertical };
+    public Changer changer;
+
+    // Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    //void Update () {
+
+    //}
+
+    void OnCollisionEnter(Collision hit)
+    {
+        //direction = new Vector3(Random.Range(-if,if), Random.Range);
+        if (hit.transform.tag == "duck")
+        {
+            DuckMovement movement = hit.gameObject.GetComponent<DuckMovement>();
+
+            if (changer == Changer.Horizontal)
+            {
+                movement.DirectionChanger(new Vector3(-1, 1, 0));
+            }
+            else if (changer == Changer.Vertical)
+            {
+                movement.DirectionChanger(new Vector3(1, -1, 0));
+            }
+        }
+    }
+}
---
+++ Duck Hunt/Assets/DuckKill.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckKill : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag == "killzone")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
